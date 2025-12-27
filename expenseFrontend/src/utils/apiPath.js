1ed4ab56c50829a256b0fa9aa46cea9fec70e23c
Khan Saheruddin <khansaheruddin06@gmail.com>
2025-12-27 15:46:01 +0530

    Update apiPath.js

--- expenseFrontend/src/utils/apiPath.js
+++ expenseFrontend/src/utils/apiPath.js
@@ -1,28 +1,30 @@
-export const BASE_URL = 'http://localhost:8000'
+// Base URL comes from environment (DEV / PROD)
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-// utils/apiPahts.js
+// API Paths
 export const API_PATHS = {
-    AUTH: {
-        LOGIN: '/api/v1/auth/login',
-        REGISTER: '/api/v1/auth/register',
-        GET_USER_INFO: '/api/v1/auth/getUser'
-    },
-    DASHBOARD: {
-        GET_DATA: '/api/v1/dashboard',
-    },
-    INCOME: {
-        ADD_INCOME: '/api/v1/income/add',
-        GET_ALL_INCOME: '/api/v1/income/get',
-        DELETE_INCOME: (incomeId) => `/api/v1/income/${incomeId}`,
-        DONWLOAD_INCOME: '/api/v1/income/downloadExcel'
-    },
-    EXPENSE: {
-        ADD_EXPENSE: '/api/v1/expense/add',
-        GET_ALL_EXPENSE: '/api/v1/expense/get',
-        DELETE_EXPENSE: (expenseId) => `/api/v1/expense/${expenseId}`,
-        DONWLOAD_EXPENSE: '/api/v1/expense/downloadExcel'
-    },
-    IMAGE: {
-        UPLOAD_IMAGE: '/api/v1/auth/upload-image'
-    }
-}
+  AUTH: {
+    LOGIN: '/api/v1/auth/login',
+    REGISTER: '/api/v1/auth/register',
+    GET_USER_INFO: '/api/v1/auth/getUser',
+    UPLOAD_IMAGE: '/api/v1/auth/upload-image'
+  },
+
+  DASHBOARD: {
+    GET_DATA: '/api/v1/dashboard'
+  },
+
+  INCOME: {
+    ADD_INCOME: '/api/v1/income/add',
+    GET_ALL_INCOME: '/api/v1/income/get',
+    DELETE_INCOME: (incomeId) => `/api/v1/income/${incomeId}`,
+    DOWNLOAD_INCOME: '/api/v1/income/downloadExcel'
+  },
+
+  EXPENSE: {
+    ADD_EXPENSE: '/api/v1/expense/add',
+    GET_ALL_EXPENSE: '/api/v1/expense/get',
+    DELETE_EXPENSE: (expenseId) => `/api/v1/expense/${expenseId}`,
+    DOWNLOAD_EXPENSE: '/api/v1/expense/downloadExcel'
+  }
+};
